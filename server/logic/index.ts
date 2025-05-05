d5dedcb217125deb9a64f256ef31a24d5b1d98e2
Marc Llobet <Tofu>
2025-05-05 17:21:52 +0200

    heroku backend version 21

--- server/logic/index.ts
+++ server/logic/index.ts
@@ -4,212 +4,214 @@ import { getEntrades, getEntradesPerParaula } from './getEntrades'
 import { ABECEDARI } from './constants'
 import { filtradorItems, filtreCaractersUnics } from './filtres'
 
-export const getMillorParaula = (paraules: string[], llargada: number ) => {
-console.time('performance')
+export const getMillorParaula = (paraules: string[], llargada: number) => {
+    console.time('performance')
 
-type QuantitatIPosicio = {
-    quantitat: number
-    posicio: number
-}
-
-const getQuantitatIPosicions = (): QuantitatIPosicio[] =>
-    Array(llargada)
-        .fill(null)
-        .map((_, posicio) => ({ quantitat: 0, posicio }))
-
-const lletresAbecedari = ABECEDARI.split('')
-
-type Lletra = {
-    lletra: string
-}
-
-type RecompteLletraAmbPosicions = Lletra & {
-    total: number
-    quantitatIPosicions: QuantitatIPosicio[]
-}
-
-type RecomptePerLletra = {
-    [lletra: string]: RecompteLletraAmbPosicions
-}
-
-const recomptePerLletra = lletresAbecedari.reduce(
-    (prev, lletra) => ({
-        ...prev,
-        [lletra]: {
-            lletra,
-            quantitatIPosicions: getQuantitatIPosicions(),
-            total: 0,
-        },
-    }),
-    {} as RecomptePerLletra
-)
-
-paraules.forEach((paraula) =>
-    paraula.split('').forEach((lletra, index) => {
-        const lletraDiccionari = recomptePerLletra[lletra]
-        lletraDiccionari.quantitatIPosicions[index].quantitat++
-        lletraDiccionari.total++
-    })
-)
-
-const paraulesCaractersUnics = filtradorItems(paraules, [
-    filtreCaractersUnics,
-])
-
-const paraulesRestants = paraulesCaractersUnics
-
-const lletresOrdenades = Object.values(recomptePerLletra)
-    .map((lletra) => ({
-        ...lletra,
-        quantitatIPosicions: [...lletra.quantitatIPosicions].sort(
-            (a, b) => b.quantitat - a.quantitat
-        ),
-    }))
-    .sort((a, b) => b.total - a.total)
-
-type RecompteLletraAmbPosicio = Lletra & QuantitatIPosicio
-
-type ItemsAfegits = {
-    lletres: RecompteLletraAmbPosicio[]
-    posicionsJaUsades: number[]
-    lletresJaUsades: string[]
-}
-
-type MillorLletres = ItemsAfegits & {
-    paraulesRestants: string[]
-}
-
-const getSeguentLletra = (
-    prev: MillorLletres
-): RecompteLletraAmbPosicions | undefined => {
-    const seguentLletra = lletresOrdenades.find(
-        ({ quantitatIPosicions, lletra }) => {
-            if (prev.lletresJaUsades?.includes(lletra)) return
-
-            const seguentPosicio = quantitatIPosicions.find(({ posicio }) => {
-                if (prev.posicionsJaUsades?.includes(posicio)) return
-
-                return prev.paraulesRestants.find(
-                    (paraula) => lletra === paraula[posicio]
-                )
-            })
-            return seguentPosicio ?? undefined
-        }
-    )
-    return seguentLletra
-}
-
-const getSeguentQuantitatIPosicio = (
-    metaLletra: RecompteLletraAmbPosicions,
-    paraulesRestants: string[]
-) => {
-    const { lletra, quantitatIPosicions } = metaLletra
-    return quantitatIPosicions.find(({ posicio }) =>
-        paraulesRestants.some((paraula) => lletra === paraula[posicio])
-    )
-}
-
-type GetParaulesRestants = {
-    paraulesRestants: string[]
-    posicio: number
-    lletra: string
-}
-
-const getParaulesRestants = ({
-    paraulesRestants,
-    posicio,
-    lletra,
-}: GetParaulesRestants) =>
-    paraulesRestants.filter((paraula) => paraula[posicio] === lletra)
-
-type GetItemsAfegits = {
-    quantitat: number
-    lletra: string
-    posicio: number
-}
-
-const getItemsAfegits = (
-    { quantitat, lletra, posicio }: GetItemsAfegits,
-    prev: MillorLletres
-): ItemsAfegits => {
-    const novaLletra = {
-        lletra,
-        quantitat,
-        posicio,
+    type QuantitatIPosicio = {
+        quantitat: number
+        posicio: number
     }
 
-    const lletres = [...(prev?.lletres ?? []), novaLletra]
+    const getQuantitatIPosicions = (): QuantitatIPosicio[] =>
+        Array(llargada)
+            .fill(null)
+            .map((_, posicio) => ({ quantitat: 0, posicio }))
 
-    const posicionsJaUsades = [...(prev?.posicionsJaUsades ?? []), posicio]
+    const lletresAbecedari = ABECEDARI.split('')
 
-    const lletresJaUsades = [...(prev?.lletresJaUsades ?? []), lletra]
-
-    return {
-        lletres,
-        posicionsJaUsades,
-        lletresJaUsades,
+    type Lletra = {
+        lletra: string
     }
-}
 
-const millorLletres = Array(QUANTITAT_LLETRES)
-    .fill(null)
-    .reduce(
-        (prev: MillorLletres) => {
-            const seguentLletra = getSeguentLletra(prev)
+    type RecompteLletraAmbPosicions = Lletra & {
+        total: number
+        quantitatIPosicions: QuantitatIPosicio[]
+    }
 
-            if (!seguentLletra) return prev
+    type RecomptePerLletra = {
+        [lletra: string]: RecompteLletraAmbPosicions
+    }
 
-            const seguentQuantitatIPosicio = getSeguentQuantitatIPosicio(
-                seguentLletra,
-                prev.paraulesRestants
-            )
-
-            if (!seguentQuantitatIPosicio) return prev
-
-            const { quantitat, posicio } = seguentQuantitatIPosicio
-            const { lletra } = seguentLletra
-
-            const paraulesRestants = getParaulesRestants({
-                paraulesRestants: prev?.paraulesRestants,
-                posicio,
+    const recomptePerLletra = lletresAbecedari.reduce(
+        (prev, lletra) => ({
+            ...prev,
+            [lletra]: {
                 lletra,
-            })
-
-            const { lletres, posicionsJaUsades, lletresJaUsades } =
-                getItemsAfegits(
-                    {
-                        quantitat,
-                        lletra,
-                        posicio,
-                    },
-                    prev
-                )
-
-            return {
-                paraulesRestants,
-                lletres,
-                posicionsJaUsades,
-                lletresJaUsades,
-            }
-        },
-        {
-            paraulesRestants,
-            lletres: [],
-            posicionsJaUsades: [],
-            lletresJaUsades: [],
-        } as MillorLletres
+                quantitatIPosicions: getQuantitatIPosicions(),
+                total: 0,
+            },
+        }),
+        {} as RecomptePerLletra
     )
 
-const millorParaula = millorLletres.lletres.reduce(
-    (prev, { posicio, lletra }) => {
-        const paraula = [...prev]
-        paraula[posicio] = lletra
-        return paraula
-    },
-    [] as string[]
-)
-console.timeEnd('performance')
-return millorParaula
+    paraules.forEach((paraula) =>
+        paraula.split('').forEach((lletra, index) => {
+            const lletraDiccionari = recomptePerLletra[lletra]
+            lletraDiccionari.quantitatIPosicions[index].quantitat++
+            lletraDiccionari.total++
+        })
+    )
+
+    const paraulesCaractersUnics = filtradorItems(paraules, [
+        filtreCaractersUnics,
+    ])
+
+    const paraulesRestants = paraulesCaractersUnics
+
+    const lletresOrdenades = Object.values(recomptePerLletra)
+        .map((lletra) => ({
+            ...lletra,
+            quantitatIPosicions: [...lletra.quantitatIPosicions].sort(
+                (a, b) => b.quantitat - a.quantitat
+            ),
+        }))
+        .sort((a, b) => b.total - a.total)
+
+    type RecompteLletraAmbPosicio = Lletra & QuantitatIPosicio
+
+    type ItemsAfegits = {
+        lletres: RecompteLletraAmbPosicio[]
+        posicionsJaUsades: number[]
+        lletresJaUsades: string[]
+    }
+
+    type MillorLletres = ItemsAfegits & {
+        paraulesRestants: string[]
+    }
+
+    const getSeguentLletra = (
+        prev: MillorLletres
+    ): RecompteLletraAmbPosicions | undefined => {
+        const seguentLletra = lletresOrdenades.find(
+            ({ quantitatIPosicions, lletra }) => {
+                if (prev.lletresJaUsades?.includes(lletra)) return
+
+                const seguentPosicio = quantitatIPosicions.find(
+                    ({ posicio }) => {
+                        if (prev.posicionsJaUsades?.includes(posicio)) return
+
+                        return prev.paraulesRestants.find(
+                            (paraula) => lletra === paraula[posicio]
+                        )
+                    }
+                )
+                return seguentPosicio ?? undefined
+            }
+        )
+        return seguentLletra
+    }
+
+    const getSeguentQuantitatIPosicio = (
+        metaLletra: RecompteLletraAmbPosicions,
+        paraulesRestants: string[]
+    ) => {
+        const { lletra, quantitatIPosicions } = metaLletra
+        return quantitatIPosicions.find(({ posicio }) =>
+            paraulesRestants.some((paraula) => lletra === paraula[posicio])
+        )
+    }
+
+    type GetParaulesRestants = {
+        paraulesRestants: string[]
+        posicio: number
+        lletra: string
+    }
+
+    const getParaulesRestants = ({
+        paraulesRestants,
+        posicio,
+        lletra,
+    }: GetParaulesRestants) =>
+        paraulesRestants.filter((paraula) => paraula[posicio] === lletra)
+
+    type GetItemsAfegits = {
+        quantitat: number
+        lletra: string
+        posicio: number
+    }
+
+    const getItemsAfegits = (
+        { quantitat, lletra, posicio }: GetItemsAfegits,
+        prev: MillorLletres
+    ): ItemsAfegits => {
+        const novaLletra = {
+            lletra,
+            quantitat,
+            posicio,
+        }
+
+        const lletres = [...(prev?.lletres ?? []), novaLletra]
+
+        const posicionsJaUsades = [...(prev?.posicionsJaUsades ?? []), posicio]
+
+        const lletresJaUsades = [...(prev?.lletresJaUsades ?? []), lletra]
+
+        return {
+            lletres,
+            posicionsJaUsades,
+            lletresJaUsades,
+        }
+    }
+
+    const millorLletres = Array(QUANTITAT_LLETRES)
+        .fill(null)
+        .reduce(
+            (prev: MillorLletres) => {
+                const seguentLletra = getSeguentLletra(prev)
+
+                if (!seguentLletra) return prev
+
+                const seguentQuantitatIPosicio = getSeguentQuantitatIPosicio(
+                    seguentLletra,
+                    prev.paraulesRestants
+                )
+
+                if (!seguentQuantitatIPosicio) return prev
+
+                const { quantitat, posicio } = seguentQuantitatIPosicio
+                const { lletra } = seguentLletra
+
+                const paraulesRestants = getParaulesRestants({
+                    paraulesRestants: prev?.paraulesRestants,
+                    posicio,
+                    lletra,
+                })
+
+                const { lletres, posicionsJaUsades, lletresJaUsades } =
+                    getItemsAfegits(
+                        {
+                            quantitat,
+                            lletra,
+                            posicio,
+                        },
+                        prev
+                    )
+
+                return {
+                    paraulesRestants,
+                    lletres,
+                    posicionsJaUsades,
+                    lletresJaUsades,
+                }
+            },
+            {
+                paraulesRestants,
+                lletres: [],
+                posicionsJaUsades: [],
+                lletresJaUsades: [],
+            } as MillorLletres
+        )
+
+    const millorParaula = millorLletres.lletres.reduce(
+        (prev, { posicio, lletra }) => {
+            const paraula = [...prev]
+            paraula[posicio] = lletra
+            return paraula
+        },
+        [] as string[]
+    )
+    console.timeEnd('performance')
+    return millorParaula
 }
 
 export const getMillorEntrada = () => {
